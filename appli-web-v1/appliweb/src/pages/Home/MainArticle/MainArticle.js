c915c800bfb37c0719e8e8a714e1d008904cee11
mousqtr <49991720+mousqtr@users.noreply.github.com>
2022-04-18 00:45:50 +0200

    Improve main article

--- appli-web-v1/appliweb/src/pages/Home/MainArticle/MainArticle.js
+++ appli-web-v1/appliweb/src/pages/Home/MainArticle/MainArticle.js
@@ -15,11 +15,12 @@ export default function MainArticle () {
                 <h2>
                     Main Article
                 </h2>
-                <h4>
+                <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
                     standard dummy text ever since the 1500s, when an unknown printer took a galley 
                     of type and scrambled it to make a type specimen book.
-                </h4>
+                </p>
+                <h5>Lire la suite</h5>
             </div>
             
         </div>
