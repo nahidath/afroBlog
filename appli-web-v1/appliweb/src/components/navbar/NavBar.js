b6226fad0270d746f64008742d6e080bf4ae31bf
nahidath <36660735+nahidath@users.noreply.github.com>
2022-04-24 11:56:30 -0400

    modify navbar and search bar

--- appli-web-v1/appliweb/src/components/navbar/NavBar.js
+++ appli-web-v1/appliweb/src/components/navbar/NavBar.js
@@ -1,7 +1,8 @@
 // Import modules
-import React            from 'react';
 import { Navbar, Nav, NavDropdown, Container, Form, FormControl, Button }    from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
+import { BsSearch } from "react-icons/bs";
+import { React, useEffect, useRef, useState } from "react";
 
 // Import style
 import "./NavBar.css";
@@ -20,11 +21,27 @@ export default function NavBar (props) {
         history.push({ pathname:'/sign-up'});
     }
 
+    
+    const targetRef = useRef(null);
+    const [isHovered, setIsHovered] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+    const showSearchInput = isHovered || isFocused;
+
+    useEffect(() => {
+        targetRef.current.value = "";
+    }, [showSearchInput]);
+
+    const [isDisplay, setDisplay] = useState(false);
+    
+    
+
     return (
         <div id='navBar'>
             <Navbar bg="light" expand="lg">
                 <Container fluid>
-                    <Navbar.Brand onClick={handleHome}>Afro Blog</Navbar.Brand>
+                    <Navbar.Brand onClick={handleHome}>
+                        <img src="./logo.jpg" alt= "Afro Blog" />
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                     <Nav
@@ -32,16 +49,17 @@ export default function NavBar (props) {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
+                        <Nav.Link href="#action1">Cheveux</Nav.Link>
+                        <Nav.Link href="#action2">Maquillage</Nav.Link>
+                        <Nav.Link href="#action2">Peau</Nav.Link>
+                        {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
                         <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action5">
                             Something else here
                         </NavDropdown.Item>
-                        </NavDropdown>
+                        </NavDropdown> */}
                         <Nav.Link onClick={handleSignUp}>
                             Sign up
                         </Nav.Link>
@@ -49,12 +67,22 @@ export default function NavBar (props) {
                     <Nav className='rightPart'>
                         <Form className="d-flex">
                             <FormControl
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                                ref={targetRef} 
+                                showSearchInput={showSearchInput}
+                                style={{ display: isDisplay ? "block" : "none" }}
+                            /> 
+                            {/* <Button variant="outline-success">Search</Button> */}
+                            < BsSearch className="bs-search"
+                                size={20} 
+                                onFocus={() => setIsFocused(true)}
+                                onBlur={() => setIsFocused(false)}
+                                hover={showSearchInput} 
+                                onClick={() => setDisplay((d) => !d)}
                             />
-                            <Button variant="outline-success">Search</Button>
                         </Form>
                     </Nav>
                     </Navbar.Collapse>
