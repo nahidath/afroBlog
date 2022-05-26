483557cc69359facb281f5288b29f7212d1a185d
nahidath <36660735+nahidath@users.noreply.github.com>
2022-05-26 19:09:51 -0400

    add card grid system

--- appli-web-v1/front/src/pages/Articles/Articles.js
+++ appli-web-v1/front/src/pages/Articles/Articles.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './Articles.css';
-import {Button, Card, Tab, Tabs} from "react-bootstrap";
+import {Button, Card, Col, Row, Tab, Tabs} from "react-bootstrap";
 
 // import Filters from './Filters/Filters';
 // import ArticlesList from './ArticlesList/ArticlesList';
@@ -17,7 +17,7 @@ export default function Articles (props) {
         //     <ArticlesList />
         // </div>
        <div className="pageSection">
-           <div id="titleBand">
+           <div id="banner">
                 Hair
            </div>
            <div className="textTitle">
@@ -31,19 +31,26 @@ export default function Articles (props) {
                    className="mb-3"
                    >
                    <Tab eventKey="all" title="Tout">
-                       <Card style={{ width: '18rem' }}>
-                           <Card.Img variant="top" src="love-test.png"/>
-                           {/*<Card.Img variant="top" src={['./articles', props.article.id, props.article.image].join('/')} />*/}
-                           <Card.Body>
-                               hair article1
-                               {/*<Card.Title>{props.article.title}</Card.Title>*/}
-                               <Card.Text>
-                                   blabla hair article1
-                                   {/*{props.article.description}*/}
-                               </Card.Text>
-                               <Button variant="outline-dark">Lire la suite</Button>
-                           </Card.Body>
-                       </Card>
+                       <Row xs={1} md={2} className="g-4">
+                           {Array.from({ length: 4 }).map((_, idx) => (
+                               <Col>
+                                   <Card style={{ width: '18rem' }}>
+                                       <Card.Img variant="top" src="/love-test.png"/>
+                                       {/*<Card.Img variant="top" src={['./articles', props.article.id, props.article.image].join('/')} />*/}
+                                       <Card.Body>
+                                           <Card.Title>hair article1</Card.Title>
+                                           <Card.Subtitle className="mb-2 text-muted">Category - Date</Card.Subtitle>
+                                           {/*<Card.Title>{props.article.title}</Card.Title>*/}
+                                           <Card.Text>
+                                               blabla hair article1
+                                               {/*{props.article.description}*/}
+                                           </Card.Text>
+                                           <Button variant="outline-dark">Lire la suite</Button>
+                                       </Card.Body>
+                                   </Card>
+                               </Col>
+                           ))}
+                       </Row>
                    </Tab>
                    <Tab eventKey="haircare" title="Soin des cheveux">
                    </Tab>
