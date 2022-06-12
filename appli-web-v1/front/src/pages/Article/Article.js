20956e9e9ddf6da63ed552bc4aa4ce3f6609a013
nahidath <36660735+nahidath@users.noreply.github.com>
2022-06-11 20:33:50 -0400

    styling discover article, comment section added

--- appli-web-v1/front/src/pages/Article/Article.js
+++ appli-web-v1/front/src/pages/Article/Article.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Article.css';
 import {
   getArticle
 } from '../../articles';
 import {Divider} from "@material-ui/core";
-import {Button, Card, Col, Row} from "react-bootstrap";
-
+import {Card, Col, Form, Row} from "react-bootstrap";
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 export default function Article (props) {
     
     const article = getArticle(props.match.params.id);
-    
+    const [name,setName] = useState("");
+
     return (
         <>
         <div className='article'>
@@ -27,31 +28,52 @@ export default function Article (props) {
                 {/*<div className='articleImageDesc'>{article.image_desc}</div>*/}
                 <div className='articleContent'>{article.content}</div>
             </div>
-            <Divider />
+            <Divider variant="middle" />
             <div className='artDiscovery'>
-                Vous pourrier aussi aimez:
+                <span>VOUS POURRIEZ AUSSI AIMER</span>
                 <div className='artList'>
                     <Row className="g-4">
                         {Array.from({length: 3}).map((_, idx) => (
                             <Col>
-                                <Card style={{width: '210px', height:'420px'}}>
+                                <Card style={{width: '210px', height:'350px'}}>
                                     <Card.Img variant="top" src="/love-test.png" style={{height:'210px'}}/>
                                     {/*<Card.Img variant="top" src={['./articles', props.article.id, props.article.image].join('/')} />*/}
                                     <Card.Body>
-                                        <Card.Title>article discover</Card.Title>
+                                        <Card.Title><a href="#">article discover</a></Card.Title>
                                         <Card.Subtitle className="mb-2 text-muted">Category - Date</Card.Subtitle>
                                         {/*<Card.Title>{props.article.title}</Card.Title>*/}
-                                        <Card.Text>
-                                            blabla article discover
-                                            {/*{props.article.description}*/}
-                                        </Card.Text>
-                                        <Button variant="outline-dark">Lire la suite</Button>
                                     </Card.Body>
                                 </Card>
                             </Col>
                         ))}
                     </Row>
                 </div>
+            </div>
+            <Divider variant="middle" />
+            <div className="commentSection">
+                <Form>
+                    <Form.Group controlId="name">
+                        <Col componentClass={Form.Label} sm={2} className="label">
+                            Nom
+                        </Col>
+                        <Col sm={10} className="inputLabel">
+                            <Form.Control
+                                autoFocus
+                                type="text"
+                                name="name"
+                                onChange={(e) => setName(e.target.value)}
+                                // placeholder="Nom"
+                            />
+                        </Col>
+                    </Form.Group>
+                    {/*<FloatingLabel controlId="floatingTextarea2" label="Comments">*/}
+                    {/*    <Form.Control*/}
+                    {/*        as="textarea"*/}
+                    {/*        placeholder="Leave a comment here"*/}
+                    {/*        style={{ height: '100px' }}*/}
+                    {/*    />*/}
+                    {/*</FloatingLabel>*/}
+                </Form>
             </div>
         </div>
         </>
