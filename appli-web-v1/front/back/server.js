457bc8e4467a5d8b9a64e422d8fe455ef98176b9
nahidath <36660735+nahidath@users.noreply.github.com>
2022-07-23 22:00:17 -0400

    first test deploy

---
+++ appli-web-v1/front/back/server.js
@@ -0,0 +1,45 @@
+// App initialization
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+const router = express.Router();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+app.use(cors({ 
+    origin: "http://localhost:3000", 
+    credentials: true 
+}));
+app.use(express.json());
+
+// Server initialization
+var credentials;
+var server = require('http').createServer(credentials, app);
+const db_url = process.env.DATABASE_URL;
+
+// Mongoose initizalization
+const mongoose = require('mongoose');
+mongoose.connect(db_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connection.on('connected', () => console.log('Database connected'));
+
+// Routes
+const userRouter = require('./routes/user.route');
+const articleRouter = require('./routes/article.route');
+const commentRouter = require('./routes/comment.route');
+app.use('/user', userRouter);
+app.use('/articles', articleRouter);
+app.use('/comments', commentRouter);
+app.use("/.netlify/back/server", router) // path must route to lambda
+app.use("/", router);
+
+module.exports.handler = serverless(app);
+
+// Run the server
+server.listen(5000);
+
+
+
