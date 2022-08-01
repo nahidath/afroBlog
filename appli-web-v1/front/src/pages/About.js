6d2ebe008d6d8e97ad302cce0264b934712f6cbd
nahidath <36660735+nahidath@users.noreply.github.com>
2022-08-01 11:00:08 +0200

    Add about.js and about.css

---
+++ appli-web-v1/front/src/pages/About.js
@@ -0,0 +1,25 @@
+import "./About.css";
+
+
+
+export default function About{
+  
+  return (
+    <div className="about-wrapper">
+      <div className="aboutPic">
+        //<img src
+      </div>
+      <div className="about-content">
+        <p>
+          <h1>A propos</h1>
+          Pour vous raconter la petite histoire, j'ai donné naissance à mon blog AFROBLOG le 20 juillet 2022.
+          J'ai toujours été une passionnée de beauté, mais c'est lors de mes nombreuses fouilles et recherches, où j'ai découvert un tout autre univers de beauté dédié aux femmes noires que je me suis vraiment intéressée aux problématiques de beauté que rencontraient les femmes noires vivant en Europe.
+          Surfant sur des forums et en parlant autour de moi où je partageais mes astuces et conseils beauté, je décide quelques temps plus tard de créer quelque chose de plus intimiste qui me ressemble mais qui parlerait aussi à un grand nombre de femmes et partager avec elle à travers la toile ma vision de c'est qu'est la BEAUTE NOIRE dans sa diversité et richesse. Elle a si longtemps était occultée et relayée au second plan par nos magazines en France.
+          Aujourd'hui en parallèle avec mon activité de blogging je fais aussi du consulting et audit aupès de grandes marques de cosmétiques et de luxe. 
+          J'organise des events beauté et tiens des conférences en France et à l'étranger.
+          Ici vous êtes comme à la maison, on retire ses chaussures, on s'installe confortablement et on s'évade dans un monde où être une femme se célèbre de la tête aux pieds.
+        </p>
+      </div>
+    </div>
+  )
+}
